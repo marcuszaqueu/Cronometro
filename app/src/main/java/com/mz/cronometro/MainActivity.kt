1f8f35f83ab295f14cea27e359613377d009cd77
MZaqueu <30877800+marcuszaqueu@users.noreply.github.com>
2021-02-26 09:17:52 -0300

    Primeiro App
    Meu primeiro aplicativo criado

--- app/src/main/java/com/mz/cronometro/MainActivity.kt
+++ app/src/main/java/com/mz/cronometro/MainActivity.kt
@@ -2,10 +2,54 @@ package com.mz.cronometro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import com.mz.cronometro.databinding.ActivityMainBinding
+import android.view.LayoutInflater as ViewLayoutInflater
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var correndo = false
+    var pause:Long =0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.iniciar.setOnClickListener {
+            IniciarCronometro()
+        }
+
+        binding.pausar.setOnClickListener {
+            PausarCronometro()
+        }
+
+        binding.zerar.setOnClickListener {
+            ZerarCronometro()
+        }
+    }
+
+    private fun IniciarCronometro(){
+        if(!correndo){
+            binding.cronometro.base = SystemClock.elapsedRealtime() - pause
+            binding.cronometro.start()
+            correndo = true
+        }
+    }
+
+    private fun PausarCronometro(){
+        if (correndo){
+            binding.cronometro.stop()
+            pause = SystemClock.elapsedRealtime() - binding.cronometro.base
+            correndo = false
+        }
+    }
+
+    private fun ZerarCronometro(){
+        binding.cronometro.base = SystemClock.elapsedRealtime()
+        pause = 0
+
     }
 }
